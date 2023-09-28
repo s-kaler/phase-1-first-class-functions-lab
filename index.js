7aaee0aa5c8c606acd6607ab7ab8cdb82cb2e0ff
Sundeep <sundeep.kaler@gmail.com>
2023-09-28 12:57:51 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = driversArray => driversArray.slice(0,2);
+const returnLastTwoDrivers = driversArray => driversArray.slice(driversArray.length - 2);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = multiplier => function (fare) {return fare * multiplier};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = (driversArray, func) => driversArray = func(driversArray);
